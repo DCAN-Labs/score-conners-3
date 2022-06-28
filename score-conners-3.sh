98fed6551eb21a94bb686968fbd27a5cb3b3f871
Paul Reiners <paul.reiners@gmail.com>
2022-06-28 13:04:15 -0500

    Update score-conners-3.sh

--- score-conners-3.sh
+++ score-conners-3.sh
@@ -1,5 +1,4 @@
-source /home/faird/shared/code/external/envs/miniconda3/load_miniconda3.sh
-conda activate nibabel_env
+# Create a conda environment according to the requirements.txt file and activate that requirement.
 export PYTHONPATH=$PYTHONPATH:./src
 
 python ./src/main.py --input_file_name $1 --age $2 --sex $3 --reporter $4
